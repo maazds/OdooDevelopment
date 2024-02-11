3462d7be9b81a0aab5ff673d65af686fe1d688c7
maazds <maazaslam16@gmail.com>
2024-02-11 15:10:01 +0500

    basic addition in init, manifest,controllers and models

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Development",
+    'summary': """
+      Development of odoo Custom Addons
+      """,
+    'description': """
+        Basic Concepts of custom module develpment in odoo
+    """,
+    'author': "Maaz Aslam",
+    'website': "https://pk.linkedin.com/in/maazaslamoo7",
+    'category': 'Development',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
 
